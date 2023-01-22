d496d2f42dbdeef78f97c2d73a68f9419adfa191
Mosmain <mosmain@yandex.ru>
2023-01-22 15:06:36 +0300

    init

--- docs/ts_ver/script.js
+++ docs/ts_ver/script.js
@@ -1,5 +1,5 @@
-// burger button and menu
 var root = document.getElementsByTagName('html')[0];
+// burger button and menu
 var menuBtn = document.querySelector('.menu-btn');
 var menu = document.querySelector('.menu');
 var menuLink = document.querySelector('.menu-links');
@@ -8,10 +8,94 @@ menuBtn.addEventListener('click', function () {
     menu.classList.toggle('active');
     root.classList.toggle('overflow-hidden');
 });
-// menuLink.addEventListener("click", (event) => {
-//   if (event.target.matches("li a")) {
-//     menuBtn.classList.remove("active");
-//     menu.classList.remove("active");
-//     root.classList.remove("overflow-hidden");
-//   }
-// });
+menuLink.addEventListener('click', function (event) {
+    if (event.target.matches('li a')) {
+        menuBtn.classList.remove('active');
+        menu.classList.remove('active');
+        root.classList.remove('overflow-hidden');
+    }
+});
+// slider
+var productContainers = document.querySelectorAll('.product-container');
+var nxtBtn = document.querySelectorAll('.nxt-btn');
+var preBtn = document.querySelectorAll('.pre-btn');
+productContainers.forEach(function (item, i) {
+    var containerDimensions = item.firstElementChild.getBoundingClientRect();
+    var containerWidth = containerDimensions.width;
+    nxtBtn[i].addEventListener('click', function () {
+        item.scrollLeft += containerWidth;
+    });
+    preBtn[i].addEventListener('click', function () {
+        item.scrollLeft -= containerWidth;
+    });
+});
+// inf carousel
+var slider = document.getElementById('slider'), sliderItems = document.getElementById('slides'), prev = document.getElementById('prev'), next = document.getElementById('next');
+function slide(wrapper, items, prev, next) {
+    var posX1 = 0, posX2 = 0, posInitial, posFinal, threshold = 100, slides = items.querySelectorAll('slide'), slidesLength = slides.length, slideSize = items.querySelector('slide').offsetWidth, firstSlide = slides[0], lastSlide = slides[slidesLength - 1], cloneFirst = firstSlide.cloneNode(true), cloneLast = lastSlide.cloneNode(true), index = 0, allowShift = true;
+    // Clone first and last slide
+    items.appendChild(cloneFirst);
+    items.insertBefore(cloneLast, firstSlide);
+    wrapper.classList.add('loaded');
+    // Mouse events
+    items.onmousedown = dragStart;
+    // Touch events
+    items.addEventListener('touchstart', dragStart);
+    items.addEventListener('touchend', dragEnd);
+    items.addEventListener('touchmove', dragAction);
+    // Click events
+    prev.addEventListener('click', function () { return shiftSlide(-1, 'drag'); });
+    next.addEventListener('click', function () { return shiftSlide(1, 'drag'); });
+    // Transition events
+    items.addEventListener('transitionend', checkIndex);
+    function dragStart(e) {
+        e = e || window.event;
+        e.preventDefault();
+        posInitial = items.offsetLeft;
+        e.type == 'touchstart'
+            ? (posX1 = e.touches[0].clientX)
+            : ((posX1 = e.clientX),
+                (document.onmouseup = dragEnd),
+                (document.onmousemove = dragAction));
+    }
+    function dragAction(e) {
+        e = e || window.event;
+        e.type == 'touchmove'
+            ? ((posX2 = posX1 - e.touches[0].clientX), (posX1 = e.touches[0].clientX))
+            : ((posX2 = posX1 - e.clientX), (posX1 = e.clientX));
+        items.style.left = items.offsetLeft - posX2 + 'px';
+    }
+    function dragEnd() {
+        posFinal = items.offsetLeft;
+        posFinal - posInitial < -threshold
+            ? shiftSlide(1, 'drag')
+            : posFinal - posInitial > threshold
+                ? shiftSlide(-1, 'drag')
+                : (items.style.left = posInitial + 'px');
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+    function shiftSlide(dir, action) {
+        items.classList.add('shifting');
+        if (allowShift) {
+            !action ? (posInitial = items.offsetLeft) : null;
+            dir == 1
+                ? ((items.style.left = posInitial - slideSize + 'px'), index++)
+                : dir == -1
+                    ? ((items.style.left = posInitial + slideSize + 'px'), index--)
+                    : null;
+        }
+        allowShift = false;
+    }
+    function checkIndex() {
+        items.classList.remove('shifting');
+        index == -1
+            ? ((items.style.left = -(slidesLength * slideSize) + 'px'),
+                (index = slidesLength - 1))
+            : index == slidesLength
+                ? ((items.style.left = -(1 * slideSize) + 'px'), (index = 0))
+                : null;
+        allowShift = true;
+    }
+}
+slide(slider, sliderItems, prev, next);
--- docs/ts_ver/script.ts
+++ docs/ts_ver/script.ts
@@ -1,6 +1,6 @@
-// burger button and menu
 const root: HTMLElement = document.getElementsByTagName('html')[0]
 
+// burger button and menu
 const menuBtn: HTMLElement = document.querySelector('.menu-btn')
 const menu: HTMLElement = document.querySelector('.menu')
 const menuLink: HTMLElement = document.querySelector('.menu-links')
@@ -11,10 +11,144 @@ menuBtn.addEventListener('click', (): void => {
   root.classList.toggle('overflow-hidden')
 })
 
-// menuLink.addEventListener("click", (event) => {
-//   if (event.target.matches("li a")) {
-//     menuBtn.classList.remove("active");
-//     menu.classList.remove("active");
-//     root.classList.remove("overflow-hidden");
-//   }
-// });
+menuLink.addEventListener('click', (event: Event): void => {
+  if ((event.target as HTMLElement).matches('li a')) {
+    menuBtn.classList.remove('active')
+    menu.classList.remove('active')
+    root.classList.remove('overflow-hidden')
+  }
+})
+
+// slider
+const productContainers =
+  document.querySelectorAll<HTMLElement>('.product-container')
+const nxtBtn = document.querySelectorAll<HTMLElement>('.nxt-btn')
+const preBtn = document.querySelectorAll<HTMLElement>('.pre-btn')
+
+productContainers.forEach((item: Element, i: number): void => {
+  let containerDimensions: DOMRect =
+    item.firstElementChild.getBoundingClientRect()
+  let containerWidth: number = containerDimensions.width
+
+  nxtBtn[i].addEventListener('click', (): void => {
+    item.scrollLeft += containerWidth
+  })
+
+  preBtn[i].addEventListener('click', (): void => {
+    item.scrollLeft -= containerWidth
+  })
+})
+
+// inf carousel
+let slider: HTMLElement = document.getElementById('slider'),
+  sliderItems: HTMLElement = document.getElementById('slides'),
+  prev: HTMLElement = document.getElementById('prev'),
+  next: HTMLElement = document.getElementById('next')
+
+function slide(
+  wrapper: HTMLElement,
+  items: HTMLElement,
+  prev: HTMLElement,
+  next: HTMLElement,
+): void {
+  let posX1: number = 0,
+    posX2: number = 0,
+    posInitial: number,
+    posFinal: number,
+    threshold: number = 100,
+    slides: NodeListOf<Element> = items.querySelectorAll<HTMLElement>('slide'),
+    slidesLength: number = slides.length,
+    slideSize: number = items.querySelector<HTMLElement>('slide').offsetWidth,
+    firstSlide: Element = slides[0],
+    lastSlide: Element = slides[slidesLength - 1],
+    cloneFirst: Node = firstSlide.cloneNode(true),
+    cloneLast: Node = lastSlide.cloneNode(true),
+    index: number = 0,
+    allowShift: boolean = true
+
+  // Clone first and last slide
+  items.appendChild(cloneFirst)
+  items.insertBefore(cloneLast, firstSlide)
+  wrapper.classList.add('loaded')
+
+  // Mouse events
+  items.onmousedown = dragStart
+
+  // Touch events
+  items.addEventListener('touchstart', dragStart)
+  items.addEventListener('touchend', dragEnd)
+  items.addEventListener('touchmove', dragAction)
+
+  // Click events
+  prev.addEventListener('click', (): void => shiftSlide(-1, 'drag'))
+  next.addEventListener('click', (): void => shiftSlide(1, 'drag'))
+
+  // Transition events
+  items.addEventListener('transitionend', checkIndex)
+
+  function dragStart(e: TouchEvent): void {
+    e = e || window.event
+    e.preventDefault()
+    posInitial = items.offsetLeft
+
+    e.type == 'touchstart'
+      ? (posX1 = e.touches[0].clientX)
+      : ((posX1 = e.clientX),
+        (document.onmouseup = dragEnd),
+        (document.onmousemove = dragAction))
+  }
+
+  function dragAction(e) {
+    e = e || window.event
+
+    e.type == 'touchmove'
+      ? ((posX2 = posX1 - e.touches[0].clientX), (posX1 = e.touches[0].clientX))
+      : ((posX2 = posX1 - e.clientX), (posX1 = e.clientX))
+
+    items.style.left = items.offsetLeft - posX2 + 'px'
+  }
+
+  function dragEnd() {
+    posFinal = items.offsetLeft
+
+    posFinal - posInitial < -threshold
+      ? shiftSlide(1, 'drag')
+      : posFinal - posInitial > threshold
+      ? shiftSlide(-1, 'drag')
+      : (items.style.left = posInitial + 'px')
+
+    document.onmouseup = null
+    document.onmousemove = null
+  }
+
+  function shiftSlide(dir, action) {
+    items.classList.add('shifting')
+
+    if (allowShift) {
+      !action ? (posInitial = items.offsetLeft) : null
+
+      dir == 1
+        ? ((items.style.left = posInitial - slideSize + 'px'), index++)
+        : dir == -1
+        ? ((items.style.left = posInitial + slideSize + 'px'), index--)
+        : null
+    }
+
+    allowShift = false
+  }
+
+  function checkIndex() {
+    items.classList.remove('shifting')
+
+    index == -1
+      ? ((items.style.left = -(slidesLength * slideSize) + 'px'),
+        (index = slidesLength - 1))
+      : index == slidesLength
+      ? ((items.style.left = -(1 * slideSize) + 'px'), (index = 0))
+      : null
+
+    allowShift = true
+  }
+}
+
+slide(slider, sliderItems, prev, next)
